981c212b803fba6248d1a3f2b0852904694e26f5
Dana Kim <92856501+abcdana122@users.noreply.github.com>
2022-08-06 10:42:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,6 @@ async def show_help(ctx):
     embed.add_field(name='`!문제찾기 (문제번호)`', value="해당 문제의 정보를 불러옵니다", inline=False)
     embed.add_field(name='`!틀린문제 (백준아이디)`', value="해당 아이디 사용자의 틀린 문제를 랜덤으로 골라 추천해줍니다", inline=False)        
     embed.add_field(name='`!알고리즘 (알고리즘)`', value="해당 알고리즘이 사용되는 문제를 랜덤으로 추천해줍니다\n알고리즘은 ' / '로 구분합니다 (ex. 수학 / 문자열)")
-    #await ctx.send(embed=embed)
-    await ctx.send('도오움말')
-
-@client.command(name='주사위')
-async def roll(ctx):
-  await ctx.send('주사위를 굴립니다')
+    await ctx.send(embed=embed)
   
 client.run(os.environ['token'])
