795b2ffa0703b0a4aff46776c6f4823822c730f3
Dana Kim <92856501+abcdana122@users.noreply.github.com>
2022-08-06 12:08:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,19 +26,8 @@ def show_problem_embed(name, url, data):
         
     return embed
   
-@client.command(name='도움말')
-async def show_help(ctx):
-    embed = discord.Embed(title=":bulb: 도움말", description="관련된 명령어를 볼 수 있습니다", color=0x568A35)
-
-    embed.add_field(name='`!문제추천`', value="백준 문제를 랜덤으로 추천해줍니다.", inline=False)
-    embed.add_field(name='`!문제추천 (티어)`', value="해당 티어의 백준 문제를 랜덤으로 추천해줍니다\n티어는 알파벳 대문자 한 글자와 숫자 하나로 구성됩니다 (ex. Silver 3 -> S3)", inline=False)
-    embed.add_field(name='`!문제찾기 (문제번호)`', value="해당 문제의 정보를 불러옵니다", inline=False)
-    embed.add_field(name='`!틀린문제 (백준아이디)`', value="해당 아이디 사용자의 틀린 문제를 랜덤으로 골라 추천해줍니다", inline=False)        
-    embed.add_field(name='`!알고리즘 (알고리즘)`', value="해당 알고리즘이 사용되는 문제를 랜덤으로 추천해줍니다\n알고리즘은 ' / '로 구분합니다 (ex. 수학 / 문자열)")
-    await ctx.send(embed=embed)
-    
-@client.command(name='문제찾기')
-async def search_problem(ctx, problem):
+# 문제 찾기
+def search_problem(problem):
     try:
         url = 'https://www.acmicpc.net/problem/'+str(problem)
         
@@ -55,15 +44,30 @@ async def search_problem(ctx, problem):
         trData = tbody.find_all('tr')
         tdData = trData[0].find_all('td')
         
-        await ctx.send(embed=(show_problem_embed(name, url, tdData)))
+        return (embed=(show_problem_embed(name, url, tdData)))
 
     except:
         embed = discord.Embed(title="[!오류] 문제가 없습니다", color=0xFF0000)
-        await ctx.send(embed=embed)
+        return (embed=embed)
+
+# 도움말
+@client.command(name='도움말')
+async def show_help(ctx):
+    embed = discord.Embed(title=":bulb: 도움말", description="관련된 명령어를 볼 수 있습니다", color=0x568A35)
+
+    embed.add_field(name='`!문제추천`', value="백준 문제를 랜덤으로 추천해줍니다.", inline=False)
+    embed.add_field(name='`!문제추천 (티어)`', value="해당 티어의 백준 문제를 랜덤으로 추천해줍니다\n티어는 알파벳 대문자 한 글자와 숫자 하나로 구성됩니다 (ex. Silver 3 -> S3)", inline=False)
+    embed.add_field(name='`!문제찾기 (문제번호)`', value="해당 문제의 정보를 불러옵니다", inline=False)
+    embed.add_field(name='`!틀린문제 (백준아이디)`', value="해당 아이디 사용자의 틀린 문제를 랜덤으로 골라 추천해줍니다", inline=False)        
+    embed.add_field(name='`!알고리즘 (알고리즘)`', value="해당 알고리즘이 사용되는 문제를 랜덤으로 추천해줍니다\n알고리즘은 ' / '로 구분합니다 (ex. 수학 / 문자열)")
+    await ctx.send(embed=embed)
+
+@client.command(name='문제찾기')
+async def my_search_problem(ctx, problem):
+    await ctx.send(embed=(search_problem(problem)))
         
 @client.command(name='틀린문제')
 async def worng_random_problem(ctx, user_id):
-    await ctx.send(user_id)
     try:
         url = 'https://www.acmicpc.net/problemset?user='+user_id+'&user_solved=0'
         req = Request(url)
@@ -80,21 +84,7 @@ async def worng_random_problem(ctx, user_id):
         target = trData[x]
         problem = target.find_all('td')[0].text
         
-        url = 'https://www.acmicpc.net/problem/'+str(problem)
-        
-        req = Request(url)
-        res = urlopen(req)
-        html = res.read()
-         
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        name = soup.find_all('span')[4].text
-
-        target = soup.find('table', {'id':'problem-info', 'class':'table'})
-        
-        tbody = target.find('tbody')
-        trData = tbody.find_all('tr')
-        tdData = trData[0].find_all('td')
-        await ctx.send(embed=(show_problem_embed(name, url, tdData)))
+        await ctx.send(embed=(search_problem(problem)))
     except:
         embed = discord.Embed(title="[!오류] 틀린 문제가 없습니다", color=0xFF0000)
         return embed
