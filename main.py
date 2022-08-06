b2ef6c0238c7587a859384552f5e578f4cb2c2e2
Dana Kim <92856501+abcdana122@users.noreply.github.com>
2022-08-06 12:02:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -65,20 +65,25 @@ async def search_problem(ctx, problem):
 async def worng_random_problem(ctx, user_id):
     await ctx.send(user_id)
     try:
+        await ctx.send(1)
         url = 'https://www.acmicpc.net/problemset?user='+user_id+'&user_solved=0'
         req = Request(url)
         res = urlopen(req)
         html = res.read()
-
+         
+        await ctx.send(2)
         soup = bs4.BeautifulSoup(html, 'html.parser')
-
+        
+        await ctx.send(3)
         table = soup.find('table')
         tbody = table.find('tbody')
         trData = tbody.find_all('tr')
-
+        
+        await ctx.send(3)
         x = random.randrange(1, len(trData))
         target = trData[x]
         problem = target.find_all('td')[0].text
+        await ctx.send(4)
         await ctx.send(embed=search_problem(problem))
     except:
         embed = discord.Embed(title="[!오류] 틀린 문제가 없습니다", color=0xFF0000)
